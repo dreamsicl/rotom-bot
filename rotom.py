4aa433da41d4e0d6c4ca97ecfa9d53c6a4c7d03f
dreamsicl <vphong@ucsd.edu>
2016-11-18 18:03:40 -0800

    successfully format output for bot

--- rotom.py
+++ rotom.py
@@ -9,8 +9,9 @@ import logging
 logging.basicConfig(level=logging.INFO)
 pp = pprint.PrettyPrinter(indent=2)
 
-### grab info from pokeapi
+# grab info from pokeapi
 pokeapi = "http://pokeapi.co/api/v2/"
+
 
 def getJSON(url):
     logging.info("getJSON(): currURL=%s" % url)
@@ -18,8 +19,8 @@ def getJSON(url):
     # init request to pokeapi, set headers
     req = urllib.request.Request(url,
                                  data=None,
-                                 headers={'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-                                 })
+                                 headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+                                          })
     response = urllib.request.urlopen(req)
 
     data = response.read()
@@ -28,11 +29,12 @@ def getJSON(url):
     return data
 
 
-### begin bot commands
+# begin bot commands
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
 There are a number of utility commands being showcased here.'''
 bot = commands.Bot(command_prefix='!', description=description)
+
 
 @bot.event
 async def on_ready():
@@ -41,13 +43,15 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
+
 @bot.command()
-async def add(left : int, right : int):
+async def add(left: int, right: int):
     """Adds two numbers together."""
     await bot.say(left + right)
 
+
 @bot.command()
-async def roll(dice : str):
+async def roll(dice: str):
     """Rolls a dice in NdN format."""
     try:
         rolls, limit = map(int, dice.split('d'))
@@ -58,21 +62,25 @@ async def roll(dice : str):
     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
     await bot.say(result)
 
+
 @bot.command(description='For when you wanna settle the score some other way')
-async def choose(*choices : str):
+async def choose(*choices: str):
     """Chooses between multiple choices."""
     await bot.say(random.choice(choices))
 
+
 @bot.command()
-async def repeat(times : int, content='repeating...'):
+async def repeat(times: int, content='repeating...'):
     """Repeats a message multiple times."""
     for i in range(times):
         await bot.say(content)
 
+
 @bot.command()
-async def joined(member : discord.Member):
+async def joined(member: discord.Member):
     """Says when a member joined."""
     await bot.say('{0.name} joined in {0.joined_at}'.format(member))
+
 
 @bot.group(pass_context=True)
 async def cool(ctx):
@@ -82,18 +90,30 @@ async def cool(ctx):
     if ctx.invoked_subcommand is None:
         await bot.say('No, {0.subcommand_passed} is not cool'.format(ctx))
 
+
 @cool.command(name='bot')
 async def _bot():
     """Is the bot cool?"""
     await bot.say('Yes, the bot is cool.')
 
+
 @bot.command()
-async def move(name : str):
+async def move(name: str):
     """ Pokemon move description """
     move = getJSON(pokeapi + "move/" + name)
+
+    # remove unnecessary keys for easier debugging
+    del move["flavor_text_entries"]
+    del move["names"]
+
     pp.pprint(move)
 
-    await bot.say("getting " + name)
+    sayMove = "Move: *" + move["name"] + "* \n" + "Damage Class: " + move["damage_class"]["name"] + " | Power: " + repr(
+        move["power"]) + " | PP: " + repr(move["pp"]) + " | Accuracy: " + repr(move["accuracy"]) + " | Type: " + move["type"]["name"]
+
+    # TODO: support multi-word move names
+
+    await bot.say(sayMove)
 
 
 bot.run('MjQ5MjUyNDkzMDg1NzY5NzI4.CxEnMA.5EJhgAM_yeHAmLDvI-676afmYLE')
