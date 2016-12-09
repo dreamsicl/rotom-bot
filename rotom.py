66d01461f0435fcd941b97291d505be1687c4d10
dreamsicl <vphong@ucsd.edu>
2016-12-08 21:52:27 -0800

    error handling

--- rotom.py
+++ rotom.py
@@ -27,7 +27,7 @@ def getJSON(url):
                                  headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
                                           })
     try: response = urllib.request.urlopen(req)
-    except urllib.request.HTTPerror as e:
+    except urllib.request.HTTPError as e:
         print(e.code)
         print(e.read())
         return False
